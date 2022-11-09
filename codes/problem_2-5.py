46af73428f9c716c9e37ff35664450129eb001d8
rahhul17 <78756375+rahhul17@users.noreply.github.com>
2022-11-09 13:02:21 +0530

    Add files via upload

---
+++ codes/problem_2-5.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+X = [1, 1]
+Y = [1]
+for i in range(20):
+    n = X[i] + X[i+1]
+    X.append(n)
+
+for i in range(1, 21):
+    m = X[i-1] + X[i+1]
+    Y.append(m)
+
+#plt.plot(X)
+plt.stem(range(len(Y)), Y)
+plt.grid()
+plt.xlabel('$n$')
+plt.ylabel('$x(n)$')
+plt.tight_layout()
+plt.savefig('/home/rahhul_17/Documents/LSP/pingala/figs/prob_2-5.pdf')
+plt.savefig('/home/rahhul_17/Documents/LSP/pingala/figs/prob_2-5.eps')
+plt.savefig('/home/rahhul_17/Documents/LSP/pingala/figs/prob_2-5.png')
+plt.show()
